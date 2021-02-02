878708f7e69bd7c3e00e5cb931aa212371a99fe1
stanbianca <55094172+stanbianca@users.noreply.github.com>
2021-02-03 01:44:27 +0200

    Added my files.

---
+++ bianca/punctul_12.R
@@ -0,0 +1,73 @@
+#am adaugat pachetul shoolmath
+
+clear_plots <- function() {
+  if(!is.null(dev.list())) {
+    dev.off()
+  }
+}
+
+#PUNCTUL 12
+suma<-function(f.X, f.Y)
+{
+  #suma
+  f.Z <- function(z) integrate(function(x,z) f.Y(z-x)*f.X(x),-Inf,Inf,z)$value
+  f.Z
+  f.Z <- Vectorize(f.Z)   
+  return(f.Z)
+}
+
+diferenta<-function(f.X, f.Y)
+{
+  #diferenta
+  f.T <-  function(z) integrate(function(x,z) f.Y(x-z)*f.X(x),-Inf,Inf,z)$value
+  f.T
+  f.T <- Vectorize(f.T)   
+}
+
+
+#EXEMPLUL 1
+f.X <- function(x) dnorm(x, 1, 0.5)
+f.Y <- function(y) dnorm(y, 1.5, 0.75)
+
+set.seed(1)
+X <- rnorm(1000,1,0.5)
+Y <- rlnorm(1000,1.5,0.75)
+Z <- X + Y
+
+hist(Z,freq=F,breaks=50, xlim=c(0,30))
+z <- seq(0,50,0.01)
+lines(z,f.X(z),lty=2,col="yellow")
+lines(z,f.Y(z),lty=2,col="green")
+lines(z,suma(f.X, f.Y)(z),lty=2,col="red")
+lines(z,diferenta(f.X, f.Y)(z),lty=2,col="blue")
+
+
+#EXEMPLUL 2
+clear_plots()
+f.X <- function(x) dunif(x, min = 0, max = 0.5)
+f.Y <- function(x) dunif(x, min = -0.2, max = 0.6)
+set.seed(1)
+X <- runif(1000,min = 0, max = 0.5)
+Y <- runif(1000,min = -0.2, max = 0.6)
+Z <- X + Y
+hist(Z,freq=F,breaks=50, xlim=c(0,30))
+z <- seq(0,50,0.01)
+lines(z,f.X(z),lty=2,col="yellow")
+lines(z,f.Y(z),lty=2,col="green")
+lines(z,suma(f.X, f.Y)(z),lty=2,col="red")
+lines(z,diferenta(f.X, f.Y)(z),lty=2,col="blue")
+
+#EXEMPLUL 3
+clear_plots()
+expX <- function(x) dexp(x, rate=1)
+expY <- function(x) dexp(x, rate=4)
+set.seed(1)
+X <- rexp(1000,1)
+Y <- rexp(1000,4)
+Z <- X + Y
+hist(Z,freq=F,breaks=50, xlim=c(0,30))
+z <- seq(0,50,0.01)
+lines(z,expX(z),lty=2,col="yellow")
+lines(z,expY(z),lty=2,col="green")
+lines(z,suma(expX, expY)(z),lty=2,col="red")
+lines(z,diferenta(expX, expY)(z),lty=2,col="blue")
